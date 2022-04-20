da58173ac3b60129ac79eff17713c889652a8065
contactharrisc2 <contactharrisc2@gmail.com>
2022-04-20 01:47:08 -0500

    removes variable pokedex

--- server.js
+++ server.js
@@ -18,7 +18,6 @@ const morgan = require("morgan");
 const pokemon = require("./models/pokemon");
 const pokeNames = getNames(pokemon);
 const pokeIndexes=[0,1,2,3,4,5,6,7,8,9,10,11];
-let pokedex = [];
 
 //////////////////////
 // Declare Middleware
